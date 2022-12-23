4024378068a5512c7ac4d024003d53655bcc2a90
Aboudoc <aboutika.reda@gmail.com>
2022-12-23 16:33:42 +0100

    initial commit

--- contracts/Contract5.sol
+++ contracts/Contract5.sol
@@ -9,7 +9,6 @@ contract Game5 {
     address threshold = 0x00FfFFfFFFfFFFFFfFfFfffFFFfffFfFffFfFFFf;
 
     function win() external {
-        console.log("Your address:", msg.sender);
         require(bytes20(msg.sender) < bytes20(threshold), "Nope. Try again!");
 
         isWon = true;
